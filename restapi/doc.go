b4e23cd3f904a69c5bfae5f52ca124f0850e618e
Matt Collinge <matt.collinge@gmail.com>
2017-03-30 10:32:27 +0100

    MC: 1st commit

---
+++ restapi/doc.go
@@ -0,0 +1,26 @@
+/*Package restapi auction
+
+reverse auction API
+
+
+    Schemes:
+      http
+      https
+    Host: localhost:3000
+    BasePath: /
+    Version: 0.0.1
+    Contact: Matt Collinge<matt.collinge@gmail.com> https://github.com/mattcollinge/auction
+
+    Consumes:
+    - application/json
+
+
+    Produces:
+    - application/json
+
+    - text/html
+
+
+swagger:meta
+*/
+package restapi
